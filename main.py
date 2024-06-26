3c3c753b436fbf4abbd47db28fdef6f3473a7b29
jgomezgadea <jgomez@spendmatters.com>
2024-06-26 10:13:10 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import dash_markdown_editor as dme
+from dash import Dash, callback, html, Input
+
+app = Dash(__name__)
+
+default_text = "# Hello World! \n## This is a subtitle\nThis is a **normal** text\n- Item 1\n- Item 2\n"
+
+app.layout = html.Div([
+    dme.MdxEditor(
+        id='mdx-editor',
+        default_text=default_text,
+    ),
+])
+
+
+@callback(Input('mdx-editor', 'text'))
+def display_output(text: str):
+    print(text)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
